08ec368e5745a982cc9a847130042ab8d69c6332
mohan <mohanreddy0532@gmail.com>
2024-07-17 16:16:30 +0530

    v2 vars sg

--- roboshop-v2/ec2.tf
+++ roboshop-v2/ec2.tf
@@ -10,10 +10,10 @@ variable "security_group_ids"{
     default = ["sg-0a23eac89d8fb6bf3"]
     }
 variable "zone_id"{
-    default = ["Z06408271IBTZUNALJ3LD"]
+    default = "Z06408271IBTZUNALJ3LD"
     }
 variable "domain_name"{
-    default = ["mdevops24.online"]
+    default = "mdevops24.online"
     }
 
 resource "aws_instance" "frontend" {
