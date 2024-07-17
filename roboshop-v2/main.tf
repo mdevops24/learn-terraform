046cd1ec3c97e1c35c382755c4b9b02837b6c908
mohan <mohanreddy0532@gmail.com>
2024-07-17 12:21:00 +0530

    v2

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance "frontend" {
-  ami           = var.ami
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.security_group_ids
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
 
   tags = {
     Name = "frontend.dev"
@@ -9,9 +9,45 @@ resource "aws_instance "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = var.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "frontend.dev.${var.domain_name}"
   type    = "A"
-  ttl     = 7
+  ttl     = 15
   records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongo" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
+
+  tags = {
+    Name = "mongo.dev"
+  }
+}
+
+resource "aws_route53_record" "mongo" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "mongo.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.mongo.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
+
+  tags = {
+    Name = "catalogue.dev"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "catalogue.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.catalogue.private_ip]
 }
--- roboshop-v2/variables.tf
+++ roboshop-v2/variables.tf
@@ -1,19 +1,7 @@
-variable "ami" {
-    default = "ami-041e2ea9402c46c32"
-  }
-
 variable "instance_type" {
-   default = "t2.micro"
-   }
-
-variable "security_group_ids"  {
-    default = "sg-0a23eac89d8fb6bf3"
-   }
-
-variable "zone_id"  {
-    default = "Z06408271IBTZUNALJ3LD"
-   }
+  default = "t3.small"
+}
 
 variable "domain_name" {
-   default = "mdevops24.online"
-   }
+  default = "mdevops24.online"
+}
