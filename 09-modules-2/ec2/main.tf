63c7ac09aba857975acfd57fb45e4668870a3cb8
mohan <mohanreddy0532@gmail.com>
2024-07-18 11:27:34 +0530

    modules-2

---
+++ 09-modules-2/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "test" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+
+  tags = {
+    Name = "test"
+  }
+}
+
+output "private_ip" {
+  value = aws_instance.test.private_ip
+}
