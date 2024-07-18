395b1c428dd9c9863a12b52487b3191a7f005716
mohan <mohanreddy0532@gmail.com>
2024-07-18 11:34:09 +0530

    modules-2

--- 09-modules-2/ec2/main.tf
+++ 09-modules-2/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-065fe091c4525f4dd"]
+  vpc_security_group_ids = ["sg-0a23eac89d8fb6bf3"]
 
   tags = {
     Name = "test"
